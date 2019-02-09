8dcec99715588dc22b114b180d560b13329ce012
Sreeparna das <sreeparna14@gmail.com>
2019-02-09 17:09:17 +0530

    commited

---
+++ Employee.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+        private int ID;
+        private string Name;
+        private string DepartmentName;
+
+
+        
+           
+            public Employee (int id,string name,string dpname)
+            {
+            ID = id;
+            Name = name;
+            DepartmentName = dpname;
+          
+            }
+        public int GetId()
+        {
+            caller1();
+            
+            return ID;
+            
+        }
+
+        public string GetName()
+        {
+            caller2();
+            return Name;
+        }
+
+        public string GetDepartmentName()
+        {
+            caller3();
+            return DepartmentName;
+        }
+     
+        public event CallDelegate caller1;
+        public event CallDelegate caller2;
+        public event CallDelegate caller3;
+
+        public int update(int id)
+        {
+            ID = id;
+            return ID;
+        }
+
+        public string update(string name)
+        {
+            Name= name;
+            return Name;
+        }
+
+        public void update()
+        {
+            Console.WriteLine("Update employee department:");
+            DepartmentName = Console.ReadLine();
+            Console.WriteLine("Updated Employee department name"+DepartmentName);
+        }
+
+
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;string name;string dpname;
+            Console.WriteLine("Enter the Employee Id:");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Employee name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the Department Name");
+            dpname = Console.ReadLine();
+            void pId()
+            {
+                Console.WriteLine("GetId() is called");
+            }
+             void pName()
+            {
+                Console.WriteLine("GetName() is called");
+            }
+           void pdpname()
+            {
+                Console.WriteLine("GetDepartmentName() is called");
+            }
+            Employee emp = new Employee(id, name, dpname);
+            emp.caller1 += new CallDelegate(pId);
+            emp.caller2 += new CallDelegate(pName);
+            emp.caller3 += new CallDelegate(pdpname);
+            Console.WriteLine("Employee ID:"+ emp.GetId());
+            
+            Console.WriteLine("Employee Name:"+emp.GetName());
+            Console.WriteLine("Employee Department Name:"+emp.GetDepartmentName());
+
+            Console.WriteLine(" Update Employee Id");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Updated Employee ID is:"+emp.update(id));
+
+            Console.WriteLine("Update Employee name");
+            name = Console.ReadLine();
+            
+            Console.WriteLine("Updated Employee name is:" + emp.update(name));
+
+            emp.update();
+
+          
+
+            
+
+           
+
+
+
+
+
+
+        }
+    }
+}
